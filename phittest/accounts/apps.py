96ef9f9775e05f87af726818467195509edb17c4
crystalshin22 <crystal.shin@wustl.edu>
2022-02-28 00:36:30 -0600

    initial file structure setup

--- phittest/accounts/apps.py
+++ phittest/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PhittestConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'phittest'
+    name = 'accounts'
