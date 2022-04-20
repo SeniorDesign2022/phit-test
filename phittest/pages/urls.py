635c423bd90ca034cc95d389b43d4f3caee6eb5e
crystalshin22 <crystal.shin@wustl.edu>
2022-04-19 22:31:29 -0500

    progress

--- phittest/pages/urls.py
+++ phittest/pages/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
+    path('dashboard/', views.CalendarView.as_view(), name='dashboard'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('survey/', views.survey_view, name='survey'),
     path('results/', views.ChartView.as_view(), name='results'),
